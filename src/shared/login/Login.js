fba3bef7aceb78a1ad665ccb81d4644b108ca045
mohammadjakirkhan4928 <mdjakirkahn4928@gmail.com>
2024-11-16 17:37:47 -0800

    Resolve merge conflicts

--- src/shared/login/Login.js
+++ src/shared/login/Login.js
@@ -61,7 +61,7 @@ const Login = () => {
   };
 
   return (
-    <section className="min-h-screen bg-gray-50">
+    <section className=" bg-gray-50">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full max-w-md p-8 bg-white rounded-xl shadow-lg space-y-6">
           <h2 className="text-2xl font-bold text-center text-gray-800">Welcome Back</h2>
